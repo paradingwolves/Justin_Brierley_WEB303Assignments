44c092f7f2c7b3fdbf000eebe74cdab8871b9036
paradingwolves <91489058+paradingwolves@users.noreply.github.com>
2022-09-15 15:57:13 -0400

    Update assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -4,23 +4,19 @@
 	W0724299
 */
 $(document).ready(function() {
-	/* $('#yearly-salary' && '#percent').on('change', function(evt){
-		let salary = document.getElementById('yearly-salary').value;
-		let percent = document.getElementById('percent').value;
-		let moneyToSpend = salary * percent / 100 .toFixed(2);
-		let amount = document.getElementById('amount');
-		amount.textContent = "$" + moneyToSpend;
-		console.log(moneyToSpend);
+	/* $('#yearly-salary' && '#percent').on('change', function(){
+		let $salary = $('#yearly-salary').val();
+		let $percent = $('#percent').val();
+		let moneyToSpend = $salary * $percent / 100 .toFixed(2);
+		$('span').text('$' + moneyToSpend);
 	}) */
 
 	// The difference between using 'keyup' and 'change' is that 'keyup' updates the value when you enter a number
 	// whereas 'change' you have to click outside the input field for the amount to update
-	$('#yearly-salary' && '#percent').on('keyup', function(evt){
-		let salary = document.getElementById('yearly-salary').value;
-		let percent = document.getElementById('percent').value;
-		let moneyToSpend = salary * percent / 100 .toFixed(2);
-		let amount = document.getElementById('amount');
-		amount.textContent = "$" + moneyToSpend;
-		/* console.log(moneyToSpend); */
+	$('#yearly-salary' && '#percent').on('keyup', function(){
+		let $salary = $('#yearly-salary').val();
+		let $percent = $('#percent').val();
+		let moneyToSpend = $salary * $percent / 100 .toFixed(2);
+		$('span').text('$' + moneyToSpend);
 	})
 })
